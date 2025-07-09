615a07e723f4f463182dd1d18d880b8d5f641d2f
caiozanetti <40394903+CAIOZANETTI@users.noreply.github.com>
2025-07-09 11:58:04 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,17 @@
+def processar_features_categoricas(df: pd.DataFrame, colunas: list) -> pd.DataFrame:
+    """
+    Converte colunas categóricas em colunas numéricas (one-hot encoding).
 
+    Args:
+        df (pd.DataFrame): O DataFrame de entrada.
+        colunas (list): Uma lista com os nomes das colunas categóricas a serem processadas.
+
+    Returns:
+        pd.DataFrame: Um novo DataFrame com as colunas categóricas transformadas.
+    """
+    df_copy = df.copy()
+    
+    # pd.get_dummies é a função perfeita para essa tarefa
+    df_com_dummies = pd.get_dummies(df_copy, columns=colunas, prefix_sep='_')
+    
+    return df_com_dummies
